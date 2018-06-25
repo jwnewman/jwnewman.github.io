17b5a9a5bb733c9e814fb6f13651a73c5b1a2655
Jess Newman <thesauritis@gmail.com>
2018-06-25 14:18:00 -0700

    Move appending logic to its own function.

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ var TUNE_NAMES = [];
     	// If running on the filesystem, ajax won't work, so populate some test data.
     	if (window.location.protocol === "file:") {
     		TUNE_NAMES = ["meeting of the minds", "spider jive"];
+    		append_tunes_to_dom();
     	} else {
     		// Iterate over the tunes in the tune directory and add them to a list.
 	    	$.ajax({
@@ -31,14 +32,18 @@ var TUNE_NAMES = [];
 	    			$(data).find("a:contains(.mp3)").each(function() {
 	    				var filename = this.href.replace(window.location, "").replace("https://", "");
 	    				TUNE_NAMES.push(unescape(filename.substr(0, filename.indexOf('.'))));
-	    				$('#tunes').append("<audio id=" + filename.substr(0, ));
+	    				append_tunes_to_dom();
 	    			});
+
 	    		}
 	    	});
 	    }
-	    // Create an audio element for each tune. The name of the element is the name of the
-	    // mp3 file, minus the extension.
-	    for (var i = 0; i < TUNE_NAMES.length; ++i) {
+    }
+
+    // Create an audio element for each tune. The name of the element is the name of the
+	// mp3 file, minus the extension.
+    function append_tunes_to_dom() {
+    	for (var i = 0; i < TUNE_NAMES.length; ++i) {
 	    	$('#tunes').append(
 	    		"<audio id='" + TUNE_NAMES[i] + "'>"
 	    			+ "<source src='" + TUNES_DIRECTORY + TUNE_NAMES[i] + ".mp3'>"
