0dadb5559210b2228a1f2aa425faf3fde2765fc5
Jessica Newman <jwnewman@mrsparkles.mtv.corp.google.com>
2018-06-25 14:06:53 -0700

    Create audio elements in the DOM for each mp3 file.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var TUNES_DIRECTORY = "/tunes/";
+var TUNES_DIRECTORY = "tunes/";
+var TUNE_NAMES = [];
 
 (function($) {
 
@@ -17,16 +18,33 @@ var TUNES_DIRECTORY = "/tunes/";
         return false;
     });
 
+    // Load all the audio files in the tunes directory into the DOM.
     function load_audio() {
-    	$.ajax({
-    		url: TUNES_DIRECTORY,
-    		success: function(data) {
-    			$(data).find("a:contains(.mp3)").each(function() {
-    				var filename = this.href.replace(window.location, "").replace("https://", "");
-    				console.log(filename);
-    			});
-    		}
-    	});
+    	// If running on the filesystem, ajax won't work, so populate some test data.
+    	if (window.location.protocol === "file:") {
+    		TUNE_NAMES = ["meeting of the minds", "spider jive"];
+    	} else {
+    		// Iterate over the tunes in the tune directory and add them to a list.
+	    	$.ajax({
+	    		url: '/'+TUNES_DIRECTORY,
+	    		success: function(data) {
+	    			$(data).find("a:contains(.mp3)").each(function() {
+	    				var filename = this.href.replace(window.location, "").replace("https://", "");
+	    				TUNE_NAMES.push(unescape(filename.substr(0, filename.indexOf('.'))));
+	    				$('#tunes').append("<audio id=" + filename.substr(0, ));
+	    			});
+	    		}
+	    	});
+	    }
+	    // Create an audio element for each tune. The name of the element is the name of the
+	    // mp3 file, minus the extension.
+	    for (var i = 0; i < TUNE_NAMES.length; ++i) {
+	    	$('#tunes').append(
+	    		"<audio id='" + TUNE_NAMES[i] + "'>"
+	    			+ "<source src='" + TUNES_DIRECTORY + TUNE_NAMES[i] + ".mp3'>"
+    			+ "</audio>"
+    		);
+	    }
     }
 
 })(jQuery);
