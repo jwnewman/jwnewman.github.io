8966cacb125cffd794e4658aae23f0f3134f97f6
Jessica Newman <jwnewman@mrsparkles.mtv.corp.google.com>
2018-06-25 16:04:01 -0700

    Implement toggling behavior for tune-playing.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 var TUNES_DIRECTORY = "tunes/";
 var TUNE_NAMES = [];
+var MOST_RECENTLY_PLAYED = "";
 
 (function($) {
 
     "use strict";    
 
-    $(document).ready(load_audio);
+    $(document).ready(loadAudio);
 
   
     $('.navigation').singlePageNav({
@@ -18,16 +19,44 @@ var TUNE_NAMES = [];
         return false;
     });
 
+    $('.fa-play-circle-o').click(function(event) {
+        var $target = $(event.target);
+        var tune = $target.data("tune");
+        var audioElement = document.getElementById(tune);
+        if (isPlaying(audioElement)) {
+            audioElement.pause();
+            audioElement.currentTime = 0;
+            audioElement.className="fa fa-play-circle-o"
+        }
+        else {
+            // Pause any playing audio.
+            var recentlyPlayedAudioElement = document.getElementById(MOST_RECENTLY_PLAYED);
+            if (isPlaying(recentlyPlayedAudioElement)) {
+                recentlyPlayedAudioElement.pause();
+                recentlyPlayedAudioElement.currentTime = 0;
+                recentlyPlayedAudioElement.className="fa fa-play-circle-o"
+            }
+            audioElement.play();
+            MOST_RECENTLY_PLAYED = tune;
+            audioElement.className="fa fa-play-circle"
+            console.log(audioElement.className);
+        }
+    });
+
+    function isPlaying(audioElement) {
+        return audioElement !== null && audioElement.duration > 0 && !audioElement.paused;
+    }
+
     // Load all the audio files in the tunes directory into the DOM.
-    function load_audio() {
+    function loadAudio() {
         // If running on the filesystem, ajax won't work, so populate some test data.
         if (window.location.protocol === "file:") {
             TUNE_NAMES = ["meeting of the minds", "spider jive", "avery's jig"];
-            append_tunes_to_dom();
+            appendTunesToDom();
         } else {
             // Iterate over the tunes in the tune directory and add them to a list.
             $.ajax({
-                url: '/'+TUNES_DIRECTORY,
+                url: '/' + TUNES_DIRECTORY,
                 success: function(data) {
                     $(data).find("a:contains(.mp3)").each(function() {
                         var filename = this.href.replace(window.location, "").replace("https://", "");
@@ -35,15 +64,15 @@ var TUNE_NAMES = [];
                             unescape(filename.substr(0, filename.indexOf('.')))
                         );
                     });
-                    append_tunes_to_dom();
+                    appendTunesToDom();
                 }
             });
         }
     }
 
     // Create an audio element for each tune. The name of the element is the name of the
-    // mp3 file, minus the extension, and stripped of apostrphes.
-    function append_tunes_to_dom() {
+    // mp3 file, minus the extension, and stripped of apostrophes.
+    function appendTunesToDom() {
         for (var i = 0; i < TUNE_NAMES.length; ++i) {
             $('#tunes').append(
                 "<audio id='" + TUNE_NAMES[i].replace("'", "") + "'>"
